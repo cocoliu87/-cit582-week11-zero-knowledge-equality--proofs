66b0901da59eb620a7a3134b72485ec407bc7038
Coco Liu <yingqiul@seas.upenn.edu>
2022-04-02 21:45:01 -0700

    ENC messages

--- ZK_equality.py
+++ ZK_equality.py
@@ -8,13 +8,14 @@ def ZK_equality(G,H):
     r2 = Secret(utils.get_random_num(bits=128))
 
     #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
-    m1, m2 = 1, 1
+    n = utils.get_random_num(bits=128)
+    m1, m2 = Secret(n), Secret(n)
 
     C1 = r1.value * G
-    C2 = m1 * G + r1.value * H
+    C2 = m1.value * G + r1.value * H
 
     D1 = r2.value * G
-    D2 = m2 * G + r2.value * H
+    D2 = m2.value * G + r2.value * H
 
     #Generate a NIZK proving equality of the plaintexts
     stmt = DLRep(C1, r1*G) & DLRep(C2, r1*H+m1*G) & DLRep(D1, r2*G) & DLRep(D2, r2*H+m2*G)
